363d2f0f79bb4893317f09ec567d7ab08a939ad1
xanf-code <darshanaswath@gmail.com>
2021-05-23 18:27:06 +0530

    request

--- routes/clean_screener.js
+++ routes/clean_screener.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const axios = require("axios");
+var request = require("request");
 const Screener = require("../models/screener_model");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
     try {
         await Screener.collection.drop().then(() => {
-            axios.get("https://insidershibu.herokuapp.com/screener/scrapescreener").then(() => {
-                res.send({
-                    type: "DB Deleted",
-                    status: 200,
-                });
+            request("https://insidershibu.herokuapp.com/screener/scrapescreener");
+            res.send({
+                type: "DB Deleted",
+                status: 200,
             });
         });
     } catch (e) {
