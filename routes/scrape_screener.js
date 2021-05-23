f4776beeda2ee952ef3354bc304b2e89f00c669a
xanf-code <darshanaswath@gmail.com>
2021-05-23 20:10:34 +0530

    new

--- routes/scrape_screener.js
+++ routes/scrape_screener.js
@@ -28,7 +28,20 @@ var cache = (duration) => {
 //ScrapeData
 router.get("/scrape", async (req, res) => {
     try {
-        await request(url, (error, response, html) => {
+        await request({
+            url: url,
+            headers: {
+                'accept': 'application/json, text/javascript, */*; q=0.01',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=0.9,kn;q=0.8',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
+                'x-requested-with': 'XMLHttpRequest',
+                'csrftoken': '9a2rWv4yjuVvl4fnzX2a2iHCZzPFONLxazEBHBvOx5Igis4Kcjto2zvGl7UFgPaq',
+                'cookie': '_gid=GA1.2.655310139.1621762775; _clck=1dd4e68; __stripe_mid=d751bbc4-1385-43ea-a5df-03c78e04b4e54fe3e2; __stripe_sid=ad4099d9-c538-4e6c-99c8-52d79b544709e819f5; __cf_bm=0b3298f28b6fb32705a90702032b5273f226be23-1621780485-1800-AfwNRQQFFVgw7hsNtvJsuBT+M/E4gDEyAm5GUA7PMZif73nfBUTsTXN8h7Xagldw+Y7Vl86q+swu8ULdP4Oc6pHpXekBS2ajb5Ybm/N2Kyj7IQp5o/T0zijYCHSWq65CRA==; _gat_gtag_UA_164596270_1=1; messages=.eJyLjlaKj88qzs-Lz00tLk5MT1XSMdAxMtVRCi5NTgaKpJXm5FQqFGem56WmKGTmKSQWK2SnlmWClOdkFmdkJuopxergMiMyv1QhI7EsFaY_v7QEn_LBbWUsAHAxZz4:1lkpDv:fjTWwDpfmH_8ntvZr6CYLWNsKVYzJNKobvfrYEV_bM8; csrftoken=9KPKvGYrkwVFxdG1Jcs6Yl8BivqkoN94a9rUgMpHy7IquBvomyTkYCWFE3vkQPyX; sessionid=5k39qu7ajj6s07y8tsj9ubs9m0jvxnuq; _clsk=13cxzy1|1621780619827|4|1|eus/collect; _ga=GA1.2.489329222.1621762775; _ga_FVDXLG2EZR=GS1.1.1621777303.3.1.1621780636.0',
+            }
+        }, (error, response, html) => {
             scrapefunction(error, response, html)
         }).then(() => {
             res.send({
